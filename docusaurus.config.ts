8a63a51a36c69f3d839cf0fe0900b052d01c7885
jacobusmeyer <janre.meyer@gmail.com>
2025-08-09 16:48:38 +0100

    Add GitHub Actions workflow for automatic deployment to GitHub Pages

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,15 +15,16 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://lenoapps.com',
+  url: 'https://JJPmeyer.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/lenoapps_website/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'JJPmeyer', // Usually your GitHub org/user name.
   projectName: 'lenoapps_website', // Usually your repo name.
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
