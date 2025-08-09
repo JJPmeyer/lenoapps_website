e042a2acc6f753e8d568aa73d6d1b50341eb7552
jacobusmeyer <janre.meyer@gmail.com>
2025-08-09 16:22:36 +0100

    Configure GitHub Pages: set projectName to lenoapps_website, add CNAME for lenoapps.com

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -22,8 +22,8 @@ const config: Config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'lenoapps', // Usually your GitHub org/user name.
-  projectName: 'leno-apps-website', // Usually your repo name.
+  organizationName: 'JJPmeyer', // Usually your GitHub org/user name.
+  projectName: 'lenoapps_website', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
