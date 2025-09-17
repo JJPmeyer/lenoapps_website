ee74deaa6f9ad712229d7d2f3bc0c0d8b76c16aa
jacobusmeyer <janre.meyer@gmail.com>
2025-09-17 15:49:11 +0100

    17092025 - v20 iframe bug fix

--- static/crisp-init.js
+++ static/crisp-init.js
@@ -1,8 +1,16 @@
-window.$crisp = []
-window.CRISP_WEBSITE_ID = 'fde3100b-48b0-47e8-95f1-cf6bef7762eb'
 ;(function () {
-  d = document
-  s = d.createElement('script')
+  try {
+    var params = new URLSearchParams(window.location.search)
+    var isEmbed = params.get('embed') === 'true'
+    // Do not load Crisp inside the embed view
+    if (isEmbed) return
+  } catch (e) {
+    // ignore
+  }
+  window.$crisp = []
+  window.CRISP_WEBSITE_ID = 'fde3100b-48b0-47e8-95f1-cf6bef7762eb'
+  var d = document
+  var s = d.createElement('script')
   s.src = 'https://client.crisp.chat/l.js'
   s.async = 1
   d.getElementsByTagName('head')[0].appendChild(s)
